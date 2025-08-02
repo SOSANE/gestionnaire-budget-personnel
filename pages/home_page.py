c010d7a66fed93d48a84ccf8849370e5ff03bb3c
SOS <sosanemh.pro@gmail.com>
2025-08-02 15:59:44 -0400

    github action set up

--- pages/home_page.py
+++ pages/home_page.py
@@ -3,7 +3,7 @@ from nicegui import ui
 def home():
     with ui.column().classes('w-full h-screen items-center justify-center'):
         ui.label('Bienvenue sur le gestionnaire de buget personnel')
-        ui.label('Veuillez enregistrer votre nom afin de débuter')
+        ui.label('Veuillez enregistrer votre nom afin de débuter.')
         nom = ui.input('Nom', placeholder='Sosane')
         navig = ui.button('Naviguer vers la page de budget', on_click=lambda: ui.navigate.to(f'/budget?nom={nom.value}'))
         navig.disable()
